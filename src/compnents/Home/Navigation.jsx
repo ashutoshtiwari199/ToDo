13cbf2aebdeb68e07da8e4fc9a8cbc621d2449db
Ashutosh Tiwari <ashutoshtiwari199@gmail.com>
2022-02-21 17:57:14 +0530

    Fug fix and dark theme added

--- src/compnents/Home/Navigation.jsx
+++ src/compnents/Home/Navigation.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Nav, NavItem } from 'reactstrap';
 
-const Navigation = () => {
+const Navigation = ({value}) => {
+
+  const {setThemeColor,themeColor} = value;
+
+  const handleTheme=()=>{
+    if(themeColor==="dark"){
+      setThemeColor("secondary")
+    } else{
+      setThemeColor("dark")
+    }
+
+  }
+
   return (
-    // <div>
-    <Nav pills className='bg-dark text-white' >
-      <NavItem className='px-5 py-3' >
-          <h4 className='mx-3 fw-light' >ToDoList</h4>
+    <Nav pills className= {`${value.themeColor} nav-shadow  d-flex justify-content-around`} >
+      <NavItem className='px-5 py-1 ' >
+          <h3 className='mx-3 my-1 fw-light' >ToDoList {} </h3>
+      </NavItem>
+      <NavItem className='px-5 py-3 ' >
+          <input type="checkbox" className="checkbox" id="checkbox" onChange={handleTheme} />
+          <label for="checkbox" className="label"  >
+          <i className="fas fa-moon"></i>
+          <i className='fas fa-sun'></i>
+          <div className='ball'/>
+          </label>
       </NavItem>
     </Nav>
-//   </div>
   );
 };
 
